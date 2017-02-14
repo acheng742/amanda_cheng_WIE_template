20c01cf75139dd5977b95977a3f00b8ffed529e4
Amanda <acheng742@gmail.com>
2017-02-13 21:03:36 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title></title>
+
+</head>
+
+<body>
+
+    <h1>Blank WordPress Template</h1>
+
+    <p>This is my WordPress template.</p>
+
+</body>
+
+</html>
